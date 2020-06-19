1c48f57821e5f5ca69a64198c0f9e36368a6ca9b
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-19 16:29:01 +0100

    Create REview

---
+++ src/components/reviews/Review.js
@@ -0,0 +1,21 @@
+import React, {Fragment} from 'react';
+
+const Review = ({review})=>{
+  if(!review){
+    return "Loading..."
+  }
+
+return(
+<Fragment>
+<p>{review.restaurant.name}</p>
+<p>{review.date}</p>
+<p>from {review.user.name}</p>
+
+<p>rate: {review.rate}</p>
+
+
+
+</Fragment>
+)          
+}
+export default Review;
