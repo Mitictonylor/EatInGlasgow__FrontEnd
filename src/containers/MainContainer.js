6929d9c0c5ad6417dde115f3424cec07dbe6ba83
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-19 14:17:36 +0100

    setup for the RestaurantContainers

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,16 +1,17 @@
 import React, {Fragment} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import NavigationBar from '../NavigationBar.js';
-
+import RestaurantContainer from './RestaurantContainer.js'
 
 const MainContainer = () => {
 
     return (
       <Router>
       <Fragment>
+
       <NavigationBar/>
       <Switch>
-      
+<Route path ='/restaurants' component = {RestaurantContainer}></Route>
       </Switch>
 
 
