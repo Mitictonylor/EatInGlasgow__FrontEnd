088ac6036ca4c9a4f6aee95bbcb6aed9ed7188ef
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-19 14:37:55 +0100

    add Restaurant List(still need links)

---
+++ src/components/restaurants/RestaurantList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Restaurant from './Restaurant.js'
+
+const RestaurantList = (props) =>{
+  if (!props.restaurants.lenght){
+    return 'Loading....'
+  }
+
+const restaurantsOptions = props.restaurants.map((restaurant, index) =>{
+  return (
+    <li key={index} className="component-item">
+		    <div className="component">
+		    <Restaurant restaurant={restaurant} />
+				</div>
+			</li>
+  )
+})
+
+
+
+}
+export default RestaurantList;
