355b10834ee63eb30be5f4c9b76c531a28f9dce9
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-22 13:26:58 +0100

    create the json file to practice with

--- src/components/users/UserRestaurantMap.js
+++ src/components/users/UserRestaurantMap.js
@@ -1,3 +1,16 @@
 import React from "react";
-import {Map, Marker, Popup, TileLayer} from "react-leaflet";
-import {Icon } from "leaflet"
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import * as parkData from "./data/skateboard-parks.json";
+
+export default function App(props) {
+
+  return (
+    <Map center={[45.4, -75.7]} zoom={12}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </Map>
+  );
+}
