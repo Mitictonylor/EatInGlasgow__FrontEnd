2e5e8025e0e60516df74574e7a67799a7f5ab0c2
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-22 15:42:34 +0100

    remove the map from the main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import MainContainer from './containers/MainContainer.js'
-import UserRestaurantMap from './components/users/UserRestaurantMap.js'
+
 
 class App extends Component {
   render(){
